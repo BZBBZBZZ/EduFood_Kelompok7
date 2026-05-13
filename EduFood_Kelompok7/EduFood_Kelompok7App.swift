8fe511c75b0f8b86a51ea52fa3cc58dbe2aabcc7
Nicho <nicholasleroy83@gmail.com>
2026-05-13 23:39:55 +0700

    Initial commit

---
+++ EduFood_Kelompok7/EduFood_Kelompok7App.swift
@@ -0,0 +1,17 @@
+//
+//  EduFood_Kelompok7App.swift
+//  EduFood_Kelompok7
+//
+//  Created by Nicholas Leroy kurniawan on 13/05/26.
+//
+
+import SwiftUI
+
+@main
+struct EduFood_Kelompok7App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
